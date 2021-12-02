b192ebe95e5db151b6f05e42cf2b4352ae86e298
Enstein Oddish <eoddish@ga-j5.msk.21-school.ru>
2021-12-02 11:53:15 +0300

    beginning

---
+++ vector.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eoddish <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/02 10:23:56 by eoddish           #+#    #+#             */
+/*   Updated: 2021/12/02 11:48:32 by eoddish          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VECTOR_HPP
+# define VECTOR_HPP
+
+# include <iostream>
+
+namespace ft {
+
+	template< typename T >
+	class vector< typename T > {
+
+		
+
+	}
+
+}
+
+#endif
